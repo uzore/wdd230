9091522d4bbd438c6e96ed1a1ea8a518f4ddae9a
everyklothe <everyklothe@gmail.com>
2023-02-09 10:37:42 +0100

    Just Adjustments

--- style/scripts/contact.js
+++ style/scripts/contact.js
@@ -1,21 +1,10 @@
-const kp1 = document.querySelector("#keyphrase");
-const kp2 = document.querySelector("#keyphrase2");
-const message = document.querySelector("#formmessage");
+const rating = document.getElementById("rating");
+const rangevalue = document.getElementById("r");
 
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-	if (kp1.value !== kp2.value) {
-		message.textContent = "Key Phrases DO NOT MATCH!";
-		message.style.display = "block";
-		kp2.style.backgroundColor = "#aa3333";
-		kp2.style.color = "#fff";
-		kp2.focus();
-		kp2.value = "";
-	} else {
-		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
-	}
+function displayRatingValue() {
+    rating.innerHTML = rangevalue.value;
 }
+
+rangevalue.addEventListener('change', displayRatingValue);
+rangevalue.addEventListener('input', displayRatingValue);
+
