e842379bc04be140a298385d1b5a41c6b94c3961
uzore <narchjosephs@gmail.com>
2023-10-17 10:32:39 +0100

    Addition of Darkness/Light Toggles

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,18 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		body.style.background = "#00021A";
+		body.style.color = "#ffff";
+        modeButton.textContent = "❎";
+	} else {
+		body.style.background = "#eeee";
+		body.style.color = "#00000";
+		modeButton.textContent = "☑️";
+	}
+});
