0bb7c51f9c0666f8ac59e9259fd56f9a512a716f
uzore <narchjosephs@gmail.com>
2023-12-14 05:10:52 +0100

    Scoots

---
+++ scoots/scripts/main.js
@@ -0,0 +1,70 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		body.style.background = "black";
+		body.style.color = "black";
+        modeButton.textContent = "❎";
+	} else {
+		body.style.background = "#eeee";
+		body.style.color = "#00021A";
+		modeButton.textContent = "☑️";
+	}
+});
+
+//Initialize display elements
+const visitsDisplay = document.querySelector(" .visits");
+
+//get the stored value in localStorage
+let noVisits = Number(window.localStorage.getItem("visits-ls"));
+
+//Determine if this is the first visit or display the number of visibility: 
+if (noVisits === 0){
+    // visitsDisplay.textContent = noVisits;
+	visitsDisplay.textContent = "Back so soon! Awesome!";
+} else if (noVisits === 1){
+	visitsDisplay.textContent = "You visited 1 day ago.";
+} else if (noVisits > 1) {
+	visitsDisplay.textContent = noVisits;
+}
+else {
+    visitsDisplay.textContent = "Welcome! Let's know if you have any questions";
+}
+
+//Increment the number of visits.
+noVisits++;
+
+//Store the new number of visits.
+localStorage.setItem("visits-ls", noVisits);
+
+
+
+ // Function to close the banner
+ function closeBanner() {
+    document.getElementById("banner").style.display = "none";
+  }
+
+  // Function to check if it's Monday, Tuesday, or Wednesday
+  function isMeetingDay() {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // Sunday is 0, Monday is 1, and so on.
+
+    return dayOfWeek >= 1 && dayOfWeek <= 3; // Monday, Tuesday, or Wednesday
+  }
+
+  // Function to show or hide the banner based on the day of the week
+  function updateBannerVisibility() {
+    const banner = document.getElementById("banner");
+    banner.style.display = isMeetingDay() ? "block" : "none";
+  }
+
+  // Update the banner visibility when the page loads
+  updateBannerVisibility();
+
+  // Set up an interval to check and update the banner visibility every hour
+  setInterval(updateBannerVisibility, 60 * 60 * 1000);
---
+++ scoots/scripts/members.js
@@ -0,0 +1,55 @@
+
+const url = 'https://uzore.github.io/wdd230/scoots/data/members.json';
+const cards = document.querySelector('#cards');
+
+const getCompanyData = async () => {
+  try {
+    // Fetch data from the JSON source URL
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Convert the response to a JSON object
+    const data = await response.json();
+    console.table(data);
+
+    // Call the displayCompanies function with data.companies as an argument
+    displayCompanies(data.companies);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const displayCompanies = (companies) => {
+  companies.forEach((company) => {
+    const card = document.createElement('section');
+    const companyDiv = document.createElement('div');
+    
+    companyDiv.innerHTML = `
+      <h4>${company.name}</h4>
+      <p><strong>Rental Type:</strong> ${company.type}</p>
+      <p><strong>Book:</strong> <a href="${company.website}" target="_blank">${company.website}</a></p>
+      <p><strong>Rental Price:</strong> ${company.price}</p>
+      <hr>
+    `;
+
+    const portrait = document.createElement('img');
+    portrait.setAttribute('src', company.image);
+    portrait.setAttribute('alt', `${company.name} Logo`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '300');
+    portrait.setAttribute('height', '300');
+
+
+    card.appendChild(portrait);
+
+    card.appendChild(companyDiv);
+
+    // Add the section card to the "cards" div
+    cards.appendChild(card);
+  });
+};
+
+getCompanyData();
